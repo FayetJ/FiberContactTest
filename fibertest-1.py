75acec39ab80d5e4de66b2dc90b523e89266f108
Jules FAYET FAVRET <47678578+FayetJ@users.noreply.github.com>
2025-10-23 16:58:30 +0200

    Update fibertest-1.py

--- fibertest-1.py
+++ fibertest-1.py
@@ -24,8 +24,11 @@ scene = app.scene.create()
 N, scale = 11, 0.05
 for i, j in np.ndindex((N, N)):
     x, y = scale * (i - N / 2), scale * (j - N / 2)
-    obj = scene.add("strand").at(x, 0, y).jitter()
+    obj = scene.add("strand").at(x, 1, y).jitter()
     (
+        if i = 2:
+            obj.pin().pull(5.0)
+
         obj.param.set("bend", 10)
         .set("contact-gap", 3e-3)
         .set("contact-offset", 3e-3)
@@ -42,7 +45,7 @@ bushing_body = scene.add("bushing_body").at(0,0,0)
 pin = bushing_body.pin()
 body_dir = os.path.join(codim_ipc_root, checkout_list[1])
 
-
+scene.add.invisible.wall([0, -3, 0], [0, 1, 0])
 
 scene = scene.build().report()
 scene.preview(options={"pin": False})
